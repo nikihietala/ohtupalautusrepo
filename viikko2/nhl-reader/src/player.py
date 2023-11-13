0160e30c26669696770b4de094ebd00b18c5d1d4
Niki Hietala <niki.hietala@gmail.com>
2023-11-13 18:15:51 +0200

    exercise 11, refactoring

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,7 @@ class Player:
         self.team = dict['team']
         self.goals = dict['goals']
         self.assists = dict['assists']
+        self.points = self.goals + self.assists
     
     def __str__(self):
-        return (f"{self.name}, team: {self.team}, goals: {self.goals}, assists: {self.assists}")
+        return f"{self.name:20} {self.team:5} {self.goals} + {self.assists} = {self.points}"
