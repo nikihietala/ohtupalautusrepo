69138c7f10ef1b9f5cbdb15624cc76820a9470b2
Niki Hietala <niki.hietala@gmail.com>
2023-11-13 18:34:14 +0200

    exercise 12, refactoring

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,22 +1,16 @@
-import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    nationality = "FIN"
 
-    players = []
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality(nationality)
 
-    for player_dict in response:
-        if player_dict['nationality'] == 'FIN':
-            player = Player(player_dict)
-            players.append(player)
+    print(f"Players from {nationality}\n")
 
-    players_by_points = sorted(players, key=lambda player: player.points, reverse=True)
-
-    print("Players from FIN")
-
-    for player in players_by_points:
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
@@ -9,3 +11,20 @@ class Player:
     
     def __str__(self):
         return f"{self.name:20} {self.team:5} {self.goals} + {self.assists} = {self.points}"
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.players = self.get_players()
+    
+    def get_players(self):
+        response = requests.get(self.url).json()
+        return [Player(player_dict) for player_dict in response]
+
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.players = player_reader.players
+    
+    def top_scorers_by_nationality(self, nationality):
+        players_of_x_nation = [player for player in self.players if player.nationality == nationality]
+        return sorted(players_of_x_nation, key=lambda player: player.points, reverse=True)
